df33b598bb58357330114e11a9bb2afbd77034bd
Sankichi <info@sankichi.net>
2015-09-21 00:58:09 +0900

    Enable an operator distinguish users

--- public/javascripts/operator.js
+++ public/javascripts/operator.js
@@ -1,16 +1,30 @@
 var socket = io.connect();
+var isStarted = false;
+var panel = $('#panel');
 socket.emit('operator');
-$('form').submit(function () {
-  var input = $('#message');
-  socket.emit('message from operator', input.val());
-  appendMessage(input.val());
-  input.val('');
-  return false;
-});
 socket.on('message', function (data) {
   console.log('message', data);
-  appendMessage(data.msg);
+  appendMessage(data.from, data.msg);
 });
-function appendMessage(msg) {
-  $('#logs').append($('<li>').addClass('list-group-item').text(msg));
+socket.on('connection', function (uid) {
+  console.log('connection', uid);
+  if (isStarted) {
+    panel.clone().attr('id', uid).appendTo('#panels');
+  } else {
+    panel.removeClass('hidden').attr('id', uid);
+    isStarted = true;
+  }
+  $('#' + uid).find('form').submit(function () {
+    var input = $('#' + uid + ' input');
+    socket.emit('message from operator', {
+      to: uid,
+      msg: input.val()
+    });
+    appendMessage(uid, input.val());
+    input.val('');
+    return false;
+  });
+});
+function appendMessage(from, msg) {
+  $('#' + from).find('ul').append($('<li>').addClass('list-group-item').text(msg));
 }
