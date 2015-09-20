69f6f6f0a308951e2ef9b1031349a0821f3edcce
Sankichi <info@sankichi.net>
2015-09-20 23:09:54 +0900

    Add operator's view

---
+++ public/javascripts/operator.js
@@ -0,0 +1,11 @@
+var socket = io.connect();
+$('form').submit(function(){
+  var input = $('#message');
+  socket.emit('message', input.val());
+  input.val('');
+  return false;
+});
+socket.on('message', function(msg) {
+  console.log('message', msg);
+  $('#logs').append($('<li>').addClass('list-group-item').text(msg));
+});
